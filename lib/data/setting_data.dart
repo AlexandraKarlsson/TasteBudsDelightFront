9eab3e8053c48dc40e1da76872ca34cf4582549b
Alexandra Karlsson <betruebefree@gmail.com>
2021-07-12 12:44:10 +0200

    Made settings data properties to private with getters/setters.

--- lib/data/setting_data.dart
+++ lib/data/setting_data.dart
@@ -1,10 +1,35 @@
 import 'package:flutter/material.dart';
 
 class SettingData extends ChangeNotifier {
-  String backendAddress = "10.0.2.2";
-  String backendPort = "8000";
-  String imageAddress = "10.0.2.2";
-  String imagePort= "8010";
+  String _backendAddress = "10.0.2.2";
+  String _backendPort = "8000";
+  String _imageAddress = "10.0.2.2";
+  String _imagePort= "8010";
   // String serverAddress = "127.0.0.1";
+
+  get backendAddress => _backendAddress;
+  get backendPort => _backendPort;
+  get imageAddress => _imageAddress;
+  get imagePort => _imagePort;
+
+  set backendAddress(value) {
+    _backendAddress = value;
+    notifyListeners();
+  }
+
+  set backendPort(value) {
+    _backendPort = value;
+    notifyListeners();
+  }
+
+  set imageAddress(value) {
+    _imageAddress = value;
+    notifyListeners();
+  }
+
+  set imagePort(value) {
+    _imagePort = value;
+    notifyListeners();
+  }
 
 }
